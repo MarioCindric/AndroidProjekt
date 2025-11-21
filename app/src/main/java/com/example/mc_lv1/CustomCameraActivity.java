2c912a9d231e13bc3db30b17a4984cb81a5436aa
Mario Cindric <mario.cindric@vuv.hr>
2025-11-21 14:25:47 +0100

    Dodavanje kamere

---
+++ app/src/main/java/com/example/mc_lv1/CustomCameraActivity.java
@@ -0,0 +1,14 @@
+package com.example.mc_lv1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomCameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_camera);
+    }
+}
